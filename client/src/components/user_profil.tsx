2e01f3395fadfbb41e890ede7a7c2e2b61fceae8
Keenjoo13 <bimalasapkota644@gmail.com>
2025-07-17 14:10:45 +0545

    React

---
+++ client/src/components/user_profil.tsx
@@ -0,0 +1,16 @@
+const UserProfile = () => {
+  return (
+    <div>
+      <p>
+        <span> Name</span>
+        <span> Bimala sapkota</span>
+      </p>
+      <p>
+        <span> Email</span>
+        <span> keenjosapkota11@gmail.com</span>
+      </p>
+    </div>
+  );
+};
+
+export default UserProfile;
