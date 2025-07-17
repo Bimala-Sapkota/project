d030508ec747d2767151fdd47b833f9f678f079c
Keenjoo13 <bimalasapkota644@gmail.com>
2025-07-17 14:07:15 +0545

    React

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./APP.css";
+import Greet from "./components/greet";
+
+function APP() {
+  return (
+    <div>
+      <h1>Shop Kart</h1>;
+      <Greet />
+    </div>
+  );
+}
+
+export default APP;
---
+++ client/src/components/greet.tsx
@@ -0,0 +1,11 @@
+const Greet = () => {
+  return (
+    <div>
+      <h1>Welcome to home page</h1>
+
+      <p>this is first react component</p>
+    </div>
+  );
+};
+
+export default Greet;
